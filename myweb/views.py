45c7fc467c0b62458084a0dfd938b4ef994de452
wanazmi69 <lawtheoscar@gmail.com>
2023-02-26 10:01:40 +0900

    first commit

---
+++ myweb/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from UserMessage.forms import UserForm
+from UserMessage.models import User
+
+def index(request):
+    users = User.objects.all()
+    
+    form = UserForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'status': 'success'})
+        else:
+            return JsonResponse({'status': 'errors'})
+    else:
+        form = UserForm()
+
+    context = {
+        'users': users,
+        'form': form,
+    }
+    return render(request, 'index.html', context)
+
+    
