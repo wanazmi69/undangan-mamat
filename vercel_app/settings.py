f6f66940c1c9b5732d118385f3915fee5d04595b
edihasrin <edyhasryn7@gmail.com>
2023-03-11 02:21:07 +0800

    ulang

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -92,11 +92,14 @@ WSGI_APPLICATION = 'vercel_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
 }
 
 
