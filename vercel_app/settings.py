d68958107c08d111ebfee5a0a9af1ffc19e22d91
edihasrin <edyhasryn7@gmail.com>
2023-03-11 02:51:04 +0800

    kirim

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
-from decouple import config
+# import dj_database_url
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,28 +94,28 @@ WSGI_APPLICATION = 'vercel_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'getu9153_curriculum_vitae',
-#         'USER': 'getu9153_wanazmi',
-#         'PASSWORD': 'iHu9ZSvtjRFzT_S',
-#         'HOST': '217.21.72.156',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'getu9153_message_wedding_mamat',
+        'USER': 'getu9153_wanazmi',
+        'PASSWORD': 'iHu9ZSvtjRFzT_S',
+        'HOST': '217.21.72.156',
+        'PORT': '3306',
     }
 }
 
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600,
-    conn_health_checks=True,
-)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES['default'] = dj_database_url.config(
+#     conn_max_age=600,
+#     conn_health_checks=True,
+# )
 
 
 # DATABASES = {
