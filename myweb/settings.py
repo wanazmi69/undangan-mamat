6622ebfb89f53d059e6696a7cb2a9cf1da134b55
wanazmi <99090379+wanazmi69@users.noreply.github.com>
2023-03-11 01:06:48 +0900

    Update settings.py
    eddite allowed host

--- myweb/settings.py
+++ myweb/settings.py
@@ -25,10 +25,11 @@ SECRET_KEY = 'django-insecure-zn96zr$i2z&^h4^(coex$x&r5i9vt1fuh#mv82jdwwo2-!j3hk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['scale.web.id/undangan-mamat/']
 
 INTERNAL_IPS = [
     "127.0.0.1",
+    'scale.web.id/undangan-mamat/'
 ]
 
 
