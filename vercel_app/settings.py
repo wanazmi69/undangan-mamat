66508fc9dd0df06ec943829d68dd7eaa7ce978ab
wanazmi <lawtheoscar@gmail.com>
2023-03-12 00:37:49 +0900

    staticfile againnn

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -191,7 +191,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # MEDIA_URL = '/media/'
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
 # if DEBUG:
 
 #   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
