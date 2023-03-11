833bcc231d8df9a46777b4004dbf7c85f55f3e9d
wanazmi <lawtheoscar@gmail.com>
2023-03-11 19:22:36 +0900

    tes bisa

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+# import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,7 +129,7 @@ DATABASES = {
 #     conn_max_age=600,
 #     conn_health_checks=True,
 # )
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # DATABASES = {
 
