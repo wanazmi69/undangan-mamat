c199739b1b0f313b10e3b9222e7c13ad5059bd1b
wanazmi <lawtheoscar@gmail.com>
2023-03-11 22:34:20 +0900

    add sttsic again

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     # path("__reload__/", include("django_browser_reload.urls")),
     path(r'', views.index, name="home"),
     path('admin/', admin.site.urls),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
