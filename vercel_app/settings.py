b8cefc400ab901880661f229a38b16ae2521de71
wanazmi <lawtheoscar@gmail.com>
2023-03-11 19:13:11 +0900

    tes lagi

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -171,10 +171,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATI_ROOT = BASE_DIR / "static"
+# STATI_ROOT = BASE_DIR / "static"
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     BASE_DIR, 'static',
 ]
