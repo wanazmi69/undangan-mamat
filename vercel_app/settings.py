973933b1d8519349cbe2193380b55b139c3d5541
wanazmi <lawtheoscar@gmail.com>
2023-03-11 21:25:56 +0900

    add sttsic

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -182,7 +182,7 @@ STATIC_URL = 'static/'
 # STATI_ROOT = BASE_DIR / "static"
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     BASE_DIR, 'static',
 ]
