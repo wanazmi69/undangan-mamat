24f841bdbdf4de4b2c6399b57aa595a464b33bfe
wanazmi <lawtheoscar@gmail.com>
2023-03-11 18:54:00 +0900

    add myswl

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
 pip install -r requirements.txt
-pip install pip install PyMySQL
 python3.9 manage.py tailwind install
 python3.9 manage.py collectstatic
--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,7 +130,7 @@ DATABASES = {
 #     conn_max_age=600,
 #     conn_health_checks=True,
 # )
-
+DATABASES['default'] = dj_database_url.config()
 
 # DATABASES = {
 
